cad599a5b7ec11fdacf8bb7da66da89bee8caa96
julian hahn <julianhahn01@gmail.com>
2023-01-28 22:50:12 +0100

    inital

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	let countObject: {x:number, y:number} = {x: 0, y:0} 
+
+	function onCount(){
+		countObject.x = countObject.x +1
+	}
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div>
+	<button on:click={onCount}>plus 1</button>
+	<p>{countObject.x}</p>
+</div>
+
+<style>
+</style>
