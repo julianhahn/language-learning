f941edfc3d6160e26e8b2b08bfc8421fc7d94b30
j0hndd <johndd1996@gmail.com>
2023-01-31 13:46:23 +0100

    at least ist not dead anymore

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,13 @@
-import nano from 'nano'
+ import nano from 'nano'
 export const load =(async()=>{
   const nano_instance = nano("http://admin:marvinpw!@127.0.0.1:5984")
   try {
       const response = await nano_instance.db.create('marvin')
-      return(response)
-    } catch (e) {
+      return({response})
+    } catch ({e}) {
       // failed
       return (e)
       
     }
 })
-
+ 
