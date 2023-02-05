a507113524d00cf9d2519e48c743491abd68084e
Marvin aber vom Server aus <johndd1996@gmail.com>
2023-02-05 23:05:17 +0000

    finally some puppies, good night

--- src/routes/api/copy_paste/+server.ts
+++ src/routes/api/copy_paste/+server.ts
@@ -13,19 +13,16 @@ export const POST = (async ({ request }) => {
 		const alice = await nano_instance.use('alice4');
 		const uuid = uuidv4();
 		console.log(uuid);
+		
 
 		const lgg = (await request.json()) as string;
 
-		fs.writeFile('../' + uuid + '.png', lgg, 'base64', function (err) {
-			console.log(err);
-		});
-		fs.readFile('../' + uuid + '.png', async (err, data) => {
-			if (!err) {
-				await alice.attachment.insert(uuid, uuid + '.png', data, 'image/png');
-			}
-		});
-
-		id = await alice.attachment.get(uuid, uuid + '.png');
+		fs.promises.writeFile('../couchdb/files/' + uuid + '.png', lgg, 'base64').then(()=>{
+		
+		fs.promises.readFile('../couchdb/files/' + uuid + '.png').then(async (data)=>{
+			await alice.attachment.insert(uuid, uuid + '.png', data, 'image/png')
+		})})
+		
 	} catch (error: any) {
 		console.log('\x1b[31m', 'could not post', error);
 		console.log('\x1b[31m', error?.description);
